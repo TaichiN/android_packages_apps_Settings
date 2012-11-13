1725224871d87ccf90f66cc7913638daf5e9033e
fuzz <fuzzzzzzzzzzzzzzzzzz@gmail.com>
2012-11-14 07:59:39 +0900

    Fix more

--- src/com/android/settings/inputmethod/InputMethodAndLanguageSettings.java
+++ src/com/android/settings/inputmethod/InputMethodAndLanguageSettings.java
@@ -196,11 +196,12 @@ public class InputMethodAndLanguageSettings extends SettingsPreferenceFragment
         }
 
         mVolumeKeyCursorControl = (ListPreference) findPreference(KEY_VOLUME_KEY_CURSOR_CONTROL);
-        mVolumeKeyCursorControl.setOnPreferenceChangeListener(this);
-        mVolumeKeyCursorControl.setValue(Integer.toString(Settings.System.getInt(getActivity()
-                .getContentResolver(), Settings.System.VOLUME_KEY_CURSOR_CONTROL, 0)));
-        mVolumeKeyCursorControl.setSummary(mVolumeKeyCursorControl.getEntry());
-        if (!Utils.hasVolumeRocker(getActivity())) {
+        if(mVolumeKeyCursorControl != null && Utils.hasVolumeRocker(getActivity())) {
+            mVolumeKeyCursorControl.setOnPreferenceChangeListener(this);
+            mVolumeKeyCursorControl.setValue(Integer.toString(Settings.System.getInt(getActivity()
+                    .getContentResolver(), Settings.System.VOLUME_KEY_CURSOR_CONTROL, 0)));
+            mVolumeKeyCursorControl.setSummary(mVolumeKeyCursorControl.getEntry());
+        } else {
             getPreferenceScreen().removePreference(mVolumeKeyCursorControl);
         }
 
